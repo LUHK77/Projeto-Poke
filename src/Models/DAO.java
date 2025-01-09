ead6ede05bd9a85efee109997ac79ddffd0de206
Lukas Schuch <166529822+LUHK77@users.noreply.github.com>
2025-01-08 21:54:40 -0300

    update 0.13
    atualizando o menu

---
+++ src/Models/DAO.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Aluno
+ */
+public class DAO {
+
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/dbpoke";
+    private static final String USER = "root";
+    private static final String pswd = "root";
+    
+    public static Connection getConnection(){
+      Connection connection = null;
+      try{
+        connection = DriverManager.getConnection(URL, USER,pswd);
+      } catch(SQLException erro) {
+            System.err.println("Erro ao conectar ao banco" + erro.getMessage());
+        }
+      return connection;
+    }
+}
+    
+
---
+++ src/Models/Poke/Biloga.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models.Poke;
+
+import Models.Ataque.Ataque;
+
+/**
+ *
+ * @author Aluno
+ */
+public abstract class Biloga {
+
+    private int id;
+    private String nome;
+    private String tipo;
+    private int ataqueBase;
+    private int stamina;
+    private int maxStamina;
+    private double hp;
+    private double maxHp;
+    private int cd;
+    private Ataque atkPrimario;
+    private Ataque atkSecundario;
+    private String imagePath;
+    
+    public Biloga(int id, String nome, String tipo, int ataqueBase, int stamina, double hp, String imagePath) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.ataqueBase = ataqueBase;
+        this.stamina = stamina;
+        this.hp = hp;
+        this.imagePath = imagePath;
+        this.maxHp += hp;
+        this.maxStamina += stamina;
+    }
+
+    public Biloga() {
+        //Vazio
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMaxStamina() {
+        return maxStamina;
+    }
+
+    public void setMaxStamina(int maxStamina) {
+        this.maxStamina = maxStamina;
+    }
+
+    public double getMaxHp() {
+        return maxHp;
+    }
+
+    public void setMaxHp(double maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public int getCd() {
+        return cd;
+    }
+
+    public void setCd(int cd) {
+        this.cd = cd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAtaqueBase() {
+        return ataqueBase;
+    }
+
+    public void setAtaqueBase(int ataqueBase) {
+        this.ataqueBase = ataqueBase;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+
+    public void setHp(double hp) {
+        this.hp = hp;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public abstract boolean atacar(Poke oponente);
+    
+    public abstract boolean atacar();
+
+    public boolean descansar() {
+        this.stamina += maxStamina * 0.3;
+        if (this.stamina > maxStamina) {
+            this.stamina = maxStamina;
+        } else if (this.stamina == maxStamina) {
+            return false;
+        }
+        return true;
+    }
+
+}
