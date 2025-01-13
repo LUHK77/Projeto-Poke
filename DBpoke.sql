271683da48e00c8fe3b6bd95bf2d1b75e0c35015
Lukas Schuch <166529822+LUHK77@users.noreply.github.com>
2025-01-13 18:25:46 -0300

    update v0.15

--- DBpoke.sql
+++ DBpoke.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE DBpoke
 DEFAULT CHARACTER SET UTF8MB4
 DEFAULT COLLATE UTF8MB4_GENERAL_CI; 
@@ -17,4 +16,8 @@ PRIMARY KEy(id)
 ) DEFAULT CHARSET = utf8mb4;
 
 insert into poke (id, apelido, level_poke, exp_poke, hp,stamina) values (1,'Galo',1,0,20,16);
-insert into poke (id, apelido, level_poke, exp_poke, hp,stamina) values (2,'Papaco',1,0,36,12);
+insert into poke (id, apelido, level_poke, exp_poke, hp,stamina) values (2,'Papaco',1,0,35,12);
+insert into poke (id, apelido, level_poke, exp_poke, hp,stamina) values (3,'Piracu',1,0,26,25);
+insert into poke (id, apelido, level_poke, exp_poke, hp,stamina) values (4,'Ashes',1,0,15,20);
+
+SELECT apelido, level_poke,exp_poke,hp,stamina FROM poke;
